605f8956d84b2a1c3ff96b13fc0a8eb02adaef4e
Efe <is.efeokumus@gmail.com>
2025-08-06 22:02:08 +0300

    Added a SecureField Button

---
+++ instaCloneApp/Core/Components/SecureFieldWithButton.swift
@@ -0,0 +1,18 @@
+//
+//  SecureFieldWithButton.swift
+//  instaCloneApp
+//
+//  Created by Efe on 6.08.2025.
+//
+
+import SwiftUI
+
+struct SecureFieldWithButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecureFieldWithButton()
+}
